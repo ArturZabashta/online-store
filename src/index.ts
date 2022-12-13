eec044841886f8ffbab3dbb5c6f7a3138bfd51a0
Kris <Krismail>
2022-12-13 13:36:23 +0300

    feat: add some routes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
+import './assets/styles/main.scss';
+import './route.js'
+
 console.log('hi')
--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,8 @@ const baseConfig = {
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             },
             { 
                 test: /\.ts$/i, use: 'ts-loader' 
