950546075bbd3942dccb31ed4950dc1c4aea71a4
Kris <Krismail>
2022-12-15 13:58:25 +0300

    feat: add checkbox filters

--- src/assets/ts/components/homePage.ts
+++ src/assets/ts/components/homePage.ts
@@ -2,7 +2,54 @@ export const HomeComponent = {
     render: () => {
       return `
       <div class="shop">
-        <aside class="shop__tools"></aside>
+        <aside class="shop__filters">
+          <div class='filter-item'>
+            <h3>Category</h3>
+            <div class="select">
+              <label for="checkbox-1">
+                <input value="1" id="checkbox-1" type="checkbox">
+                Checkbox #1
+              </label>
+              <label for="checkbox-1">
+                <input value="1" id="checkbox-1" type="checkbox">
+                Checkbox #1
+              </label>
+              <label for="checkbox-1">
+                <input value="1" id="checkbox-1" type="checkbox">
+                Checkbox #1
+              </label>
+              <label for="checkbox-1">
+              <input value="1" id="checkbox-1" type="checkbox">
+              Checkbox #1
+            </label>
+            <label for="checkbox-1">
+              <input value="1" id="checkbox-1" type="checkbox">
+              Checkbox #1
+            </label>
+            <label for="checkbox-1">
+              <input value="1" id="checkbox-1" type="checkbox">
+              Checkbox #1
+            </label>
+            </div>
+          </div>
+          <div class='filter-item'>
+            <h3>Brand</h3>
+            <div class="select">
+              <label for="checkbox-1">
+                <input value="1" id="checkbox-1" type="checkbox">
+                Checkbox #1
+              </label>
+              <label for="checkbox-1">
+                <input value="1" id="checkbox-1" type="checkbox">
+                Checkbox #1
+              </label>
+              <label for="checkbox-1">
+                <input value="1" id="checkbox-1" type="checkbox">
+                Checkbox #1
+              </label>
+            </div>
+          </div>
+        </aside>
         <section class="shop__items">
         <div class = 'item'></div>
         <div class = 'item'></div>
