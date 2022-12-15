ff0e5a6cbda3e0019180b6b9a3d81a93417d1330
Kris <Krismail>
2022-12-15 10:21:40 +0300

    feat: add fonts in project

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,10 @@ const baseConfig = {
                 test: /\.(png|svg|jpg|jpeg|gif|ico|mp3)$/i,
                 type: 'asset/resource',
             },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
         ],
     },
     resolve: {
