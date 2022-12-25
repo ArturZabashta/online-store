0c569b3b7ea07ca1eebca28698a07f9e993bdc9b
Zabashta_Artur <zarpost@gmail.com>
2022-12-25 20:12:39 +0300

    feat: add management of the number of products in the cart

--- src/assets/ts/components/cartpage.ts
+++ src/assets/ts/components/cartpage.ts
@@ -1,4 +1,4 @@
-import { zeroProduct,  returnAllProducts } from "../utilities/utilities";
+import { zeroProduct,  returnAllProducts, changeCarpProductCount } from "../utilities/utilities";
 import { IProduct } from "../interfaces/api-interfaces";
 import { ICart } from "../interfaces/cart-interfaces";
 import { returnCurtSum } from "../utilities/utilities";
@@ -31,8 +31,8 @@ export const CurtComponent = async () => {
       <aside class="cart__summary">
         <h2 class="summary__head"> Summary </h2>
         <article class="summary__description">
-          <p class="summary__products">Products:  <span class="summary__products_count">${cartSumCount[0]}</span> </p>
-          <p class="summary__products">Total:  <span class="summary__products_summa">${cartSumCount[1]}</span> </p>
+          <p class="summary__products">Products:  <span class="summary__products_count">${cartSumCount[1]}</span> </p>
+          <p class="summary__products">Total:  <span class="summary__products_summa">${cartSumCount[0]}</span> </p>
           <input class="summary__products_input btn" type="text" placeholder="Insert your promo code">
           <div class="summary__promocode"></div>
           <button class="summary__buy btn">Buy now</button>
@@ -43,11 +43,17 @@ export const CurtComponent = async () => {
 
   const cartItemsList: HTMLElement | null = document.querySelector('.cart__list');
   const itemsPerPage: HTMLElement | null = document.querySelector('.cart__pageitems_count');
+  const summaryCount: HTMLElement | null = document.querySelector('.summary__products_count');
+  const summarySumma: HTMLElement | null = document.querySelector('.summary__products_summa');
+
 
 
 
   
-  function renderCartProducts():void {    
+  function renderCartProducts():void {
+    if (cartItemsList) cartItemsList.innerHTML=''
+    const cartList: Array<ICart> = JSON.parse(String(localStorage.getItem('cartList'))) || [];
+    console.log('cartList renderCartProducts()=', cartList)    
     cartList.map((item: ICart, index:number)=> {
       const product = document.createElement('div');
       product.classList.add('cart__item');
@@ -66,18 +72,50 @@ export const CurtComponent = async () => {
         </p>
       </div>
       <div class="item__manager">
-        <p class="item__stock">Stock: <span class="item__stock_count">${copyAllProducts[item.id-1].stock}</span></p>
+        <p class="item__stock">Stock: <span class="item__stock_count" id="item__stock_count-${item.id}">${copyAllProducts[item.id-1].stock}</span></p>
         <div class="item__management">
-          <button class="item__management_less btn">-</button>
-          <span class="item__management_count">${item.count}</span>
-          <button class="item__management_more btn">+</button>
+          <button class="item__management_less btn" id="item__management_less-${item.id}">-</button>
+          <span class="item__management_count" id="item__management_count-${item.id}">${item.count}</span>
+          <button class="item__management_more btn" id="item__management_more-${item.id}">+</button>
         </div>
+        <p class="item__summa">€<span class="item__summa_count" id="item__summa_count-${item.id}">${item.count * item.price}</span></p>
       </div>
 
       `;
-
-      cartItemsList?.append(product)
+      cartItemsList?.append(product);
     });
+
+      const addItemBtnList: NodeListOf<HTMLButtonElement> = document.querySelectorAll('.item__management_more');
+      const removeItemBtnList: NodeListOf<HTMLButtonElement> = document.querySelectorAll('.item__management_less');
+
+      [...addItemBtnList].map((btn: HTMLButtonElement) => {
+        btn.addEventListener('click', setCartProductCount)
+      });
+      [...removeItemBtnList].map((btn: HTMLButtonElement) => {
+        btn.addEventListener('click', setCartProductCount)
+      });
+      
+      function setCartProductCount(this: HTMLButtonElement) {
+        const id = Number(this.id.slice(22));
+        const currentCountSpan = <HTMLElement>document.getElementById(`item__management_count-${id}`);
+        const currentSummaSpan = <HTMLElement>document.getElementById(`item__summa_count-${id}`);
+        const currentStock = Number(document.getElementById(`item__stock_count-${id}`)?.innerHTML);       
+        const currentPrice = copyAllProducts[id-1].price;
+        let step = 1;
+        if (this.innerHTML == '+') step = 1;
+        if (this.innerHTML == '-') step = -1;
+        
+        const isDelete = changeCarpProductCount(id, currentPrice, step, currentStock, currentCountSpan, currentSummaSpan);
+        
+        if (isDelete) {                  
+          renderCartProducts()
+        }
+
+        const updatedSumCount = returnCurtSum();
+        if (summarySumma) summarySumma.innerHTML = `${updatedSumCount[0]}`
+        if (summaryCount) summaryCount.innerHTML = `${updatedSumCount[1]}`        
+
+      }
   }
   renderCartProducts()
 
--- src/assets/ts/utilities/utilities.ts
+++ src/assets/ts/utilities/utilities.ts
@@ -175,7 +175,7 @@ export function returnCurtSum(): Array<number> {
   let count = 0;
   if (cartList && cartList.length > 0) {
     cartList.forEach(element => {
-      summa += element.price;
+      summa += element.price * element.count;
       count += element.count;
     });
   }
@@ -185,3 +185,43 @@ export function returnCurtSum(): Array<number> {
   
   return [summa, count]
 }
+
+function reFreshCartList(id: number, count: number){
+  const cartList: Array<ICart> = JSON.parse(String(localStorage.getItem('cartList')));
+  localStorage.removeItem('cartList')
+  if (cartList && cartList.length > 0) {
+    const newCartList = cartList
+    .map(element => {
+      if (element.id == id) {
+        element.count = count;
+      }
+      return element;
+    })
+    .filter(element => element.count !== 0);
+    localStorage.setItem('cartList', JSON.stringify(newCartList));
+    console.log('reFreshCartList=', newCartList)
+  }
+  returnCurtSum();
+}
+
+export function changeCarpProductCount(id: number, price:number, step: number, stock: number, countSpan: HTMLElement, summaSpan: HTMLElement): boolean {
+  console.log('id=', id, 'step=', step, 'stock=', stock)
+  let isDelete = false;
+  const count = Number(countSpan.innerHTML) + step;
+  const summa = count * price;
+  if (count == 0 ) {
+    isDelete = true; 
+    countSpan.innerHTML = `0`;
+    summaSpan.innerHTML = `0`;
+  }  
+  if (count >= 1 && count <= stock) {
+    countSpan.innerHTML = `${count}`;
+    summaSpan.innerHTML = `${summa}`;
+  }
+  if (count > stock) {
+    countSpan.innerHTML = `${stock}`;
+    summaSpan.innerHTML = `${stock * price}`;
+  }
+  reFreshCartList(id, count)
+  return isDelete;
+} 
