949b6af8db61677952cc31d3450a4e3d8aff2c00
Zabashta_Artur <zarpost@gmail.com>
2022-12-26 01:25:34 +0300

    feat: move cart coutn above the cart logo

--- src/assets/ts/add-base-html.ts
+++ src/assets/ts/add-base-html.ts
@@ -11,9 +11,11 @@ const baseHtml=  `
               <div class="header__total-curt">
                   <p class="total-curt">Total: <span class="total-sum">0</span></p>
               </div>
-              <div class="header__curt">
-                  <span class="total-count"></span>
-                  <a href="#/curt"><img src="https://img.icons8.com/color/32/null/shopping-cart--v1.png" alt="curt"/></a>
+              <div class="header__curt">                  
+                  <a href="#/curt">
+                    <span class="total-count"></span>
+                    <img class="cart-logo" src="https://img.icons8.com/color/32/null/shopping-cart--v1.png" alt="curt"/>
+                  </a>
               </div>
           </div>
       </header>
