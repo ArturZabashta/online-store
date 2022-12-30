1d5b09f86996f27963a38f3092d114eb77987a9a
Zabashta_Artur <zarpost@gmail.com>
2022-12-30 12:26:54 +0300

    refactor: update code from develop

--- src/assets/ts/components/itemdetails.ts
+++ src/assets/ts/components/itemdetails.ts
@@ -2,7 +2,7 @@
 import {returnOneProduct} from "../utilities/utilities"
 import { ICart } from "../interfaces/cart-interfaces"
 import { returnCurtSum } from "../utilities/cart-utilities";
-import { renderModal } from "../components/modal";
+import { renderModal } from "./modal";
 
 export const ItemComponent = async():Promise<void> => {
     const product = await returnOneProduct();
